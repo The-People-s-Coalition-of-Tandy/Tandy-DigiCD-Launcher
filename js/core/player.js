8c621559be6b6a2fd33824eb7323c9cf8e0fcddc
julipode <humphreythegray@gmail.com>
2025-01-30 22:06:05 -0500

    Add 909 to backups

--- js/core/player.js
+++ js/core/player.js
@@ -17,6 +17,7 @@ const elements = {
 
 const corruptionBackupFiles = {
     '777': {music: './assets/backups/777.mp3', midi: './assets/backups/777.mid'},
+    '909': {music: './assets/backups/909.mp3', midi: './assets/backups/909.mid'}
 }
 
 let isPlaying = false;
